b966d39091890d594e4d9bc338b463f80d880195
97revenge <empregos97@gmail.com>
2023-10-20 15:37:35 -0300

    BUG:O literal de objeto pode especificar apenas propriedades conhecidas e 'image' não existe no tipo '{ select?: UserSelect<DefaultArgs> | null | undefined; data: (Without<UserCreateInput, UserUncheckedCreateInput> & UserUncheckedCreateInput) | (Without<...> & UserCreateInput); }'.ts(2345)

--- server/src/product/index.ts
+++ server/src/product/index.ts
@@ -8,10 +8,11 @@ const prisma: PrismaClient = new PrismaClient();
 type Props = {
   getter: object;
   setter: object;
+  posting: object;
 };
 function ProductWare(this: Props, id: any, value: string) {
   this.getter = function () {
-    router.get("/product", async (req: Request, res: Response) => {
+    router.get("/product", async (req, res): Promise<Middleware | void> => {
       const instance = await prisma.user.findMany();
       res.status(201).json(instance);
     });
@@ -25,6 +26,24 @@ function ProductWare(this: Props, id: any, value: string) {
         },
       });
       res.status(201).json(instance);
+    });
+  };
+  this.posting = function () {
+    router.get("/product", async (req, res): Promise<Middleware | void> => {
+      const { image, price, tag, offer, br, name, description, free } =
+        req.body;
+      const instance = await prisma.user.create({
+        image: image,
+        price,
+        tag,
+        offer,
+        br,
+        name,
+        description,
+        free,
+      });
+
+      res.status(201).send({ instance });
     });
   };
 }
